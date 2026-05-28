3960a61b2e2f1955ec41a80fded8a347b468415d
Sarvesh Barnwal <barnwalsarvesh48@gmail.com>
2026-05-28 12:08:37 +0530

    Day-1 uploaded.

---
+++ Day-1/Q3.c
@@ -0,0 +1,15 @@
+//WAP to find factorial of a number.
+#include<stdio.h>
+int main(){
+    int n;
+    int fact=1;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        fact=fact*i;
+
+    }
+    printf("factorial of %d is %d ",n,fact);
+
+    return 0;
+}
