ae0f12c7cc665d93fdf5c9a6fab0b929d8c6584f
Sarvesh Barnwal <barnwalsarvesh48@gmail.com>
2026-05-29 20:41:08 +0530

    Day-2 uploaded.

--- Day-2/Q8.c
+++ Day-2/Q8.c
@@ -1,4 +1,4 @@
-// WAP to reverse a number.
+// WAP to check whether a number is palindrome.
 #include <stdio.h>
 
 int main() {
@@ -6,11 +6,15 @@ int main() {
     int m=0;
     printf("Enter the number:");
     scanf("%d",&num);
+    int n=num;
     while(num>0){
         m=m * 10 + num % 10;
         num = num/10;
     }
-        printf("the reverse of the number is %d",m);
+    if(m==n)
+        printf("the number is a palindrome.");
+    else
+        printf("the number is not a palindrome.");
 
     return 0;
 }
