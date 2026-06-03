8452200b0b2c5e094b7fafd1b5abf8e1405e02e6
Sarvesh Barnwal <barnwalsarvesh48@gmail.com>
2026-06-03 19:31:06 +0530

    Day-3 uploaded.

--- Day-0/sample.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("hello world\n");
-    printf("this is just a sample ");
-    return 0;
-}
--- Day-3.c/Assignment10.c
+++
@@ -1,6 +0,0 @@
-//WAP to print prime numbers in a range.
-#include<stdio.h>
-int main(){
-    
-    return 0;
-}
